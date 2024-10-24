5bc05421519ffc96f8d65e23d968aee3f8124b96
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-10-24 20:58:42 +0000

    se ha realizado la base de datos y los entpoints

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -36,16 +36,72 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/user', methods = ['GET'])
+def get_users():
+    users = User()
+    # aqui va a la base de datos a buscar todos los usuarios
+    users =  users.query.all()
+    # aplica el metodo serializar para responder al cliente
+    users = list(map(lambda item : item.serialize(), users))
+    # el metodo jsonfiy convierte los datos en formato json
+    return jsonify(users),200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/user/<int:theid>', methods = ["GET"])
+def get_by_user_id(theid=None):
+    # valida si hay un id
+    if theid is not None:
+        user  = User()
+        user = user.query.get(theid)
+        print(user)
+        
+       # cuando la base de data no consigue algo responde un null (None) (utilizando querys)
+        if user is not None : 
+            return jsonify(user.serialize()) , 200
+        else : 
+            return jsonify({
+                "Message" : "User is not found!"
+            }), 200  
+    
+# ESTANDAR A SEGUIR PARA RESPPUESTAS CON POST
+
+@app.route('/user', methods=['POST'])
+def add_user():
+    data  = request.json # recibe lo que el cliente (postman) envia
+    if data.get("name") is None:   # valida si recibimos las propiedades concretas del postman
+        return jsonify({"message": "user doenst exits"}), 400
+    if data.get("email") is None: 
+        return jsonify({"message": "user doenst exits"}), 400
+    # validacion de si el usuario existe
+    user = User()
+    user_email = user.query.filter_by(email = data['email']).first()
+    if user_email is None:
+        # Creamos a el usuario
+        user = User(name= data['name'], email = data["email"])
+        # se prepara para guardar en base de datos
+        db.session.add(user)
+        try:
+            # Lo guarda
+            db.session.commit()
+            return jsonify({"Message": "usuario guardado correctamente"}), 201
+        except Exception as error:
+            # echa para atras el proceso de guardar usuario (transaccion cancelada) si algun error ocurre
+            db.session.rollback()
+            return jsonify({"Message":"algo ha ocurrido "}), 404             
+    else : 
+        return jsonify({"Message": "El usuario existe"}), 400
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+
+    # return jsonify(response_body), 200
--- src/models.py
+++ src/models.py
@@ -1,19 +1,69 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
+
+
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable = True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # password = db.Column(db.String(80), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "email" : self.email
             # do not serialize the password, its a security breach
         }
+    
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False , unique = True)
+    clima = db.Column(db.String(20), nullable = True)
+    creacion_planeta = db.Column(db.String(50), nullable = True)
+
+
+class People(db.Model): 
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20), nullable = False)
+    birth = db.Column(db.String(50), nullable = True)
+    gender = db.Column(db.String(20), nullable = True)
+    height = db.Column(db.Float, nullable = True)
+    skin_color = db.Column(db.String(20),nullable = True)
+    hair_color = db.Column(db.String(20), nullable = True)
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.Integer, primary_key = True)
+    people_id = db.Column(db.Integer,db.ForeignKey('user.id'), nullable = False)
+    planet_id = db.Column(db.Integer,db.ForeignKey('planet.id'), nullable = True)
+
+# class User_Planet(db):
+#     __tablename__ = 'usuario_planeta'
+#     id = db.Column(Integer, primary_key = True)
+#     user_id = db.Column(Integer,ForeignKey('usuario.id'))
+#     user_id = db.Column(Integer, ForeignKey('planeta.id'))
+
+#     # RELACIONES PARA LOS ENDPOINTS 
+#     usuario = relationship('Usuario')
+#     planeta = relationship('Planeta ')
+
+
+
+# class Usuario_Personaje(db):
+#     __tablename__ = 'usuario_personaje'
+#     id = db.Column(Integer, primary_key = True)
+#     usuario_id = db.Column(Integer, ForeignKey('usuario.id'))
+#     personaje_id = db.Column(Integer, ForeignKey('personaje.id'))
+
+#     # RELACIONES PARA LOS ENDPOINTS   
+#     usuario = relationship('Usuario')
+#     personaje = relationship('Personaje')
